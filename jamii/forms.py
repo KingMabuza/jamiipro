04b171d853df82c49309922c97493361533f52b5
Jakote <jakotelejaha@gmail.com>
2021-08-27 22:55:02 +0200

    27Aug night with Jakote commit

--- jamii/forms.py
+++ jamii/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Exporter
 
 
 class UserRegisterForm(UserCreationForm):
@@ -8,4 +9,15 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = '__all__'
+
+class ExportRegisterForm(forms.ModelForm):
+    
+    class Meta:
+        model = Exporter
+        fields = ["Export_Industry","Export_From", "Export_To"]
+
+# class ProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = Profile
+#         fields = ["level_of_knowledge","domain_of_Knowledge","type_of_paper"]
